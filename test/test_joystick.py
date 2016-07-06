81f7a013616fd829a9b6b5ef43aba1a07c14f39d
Somchai Somphadung <somchaisomph@gmail.com>
2016-07-06 12:33:47 +0700

    Update test_joystick.py

--- test/test_joystick.py
+++ test/test_joystick.py
@@ -1,1 +1,19 @@
-#Nothin to write now.
+
+import signal
+import time
+from gadgets.joysticks import JoyStick2D
+
+def signal_handler(signal,frame):
+	global stop_flag
+	stop_flat = True
+
+'''
+usedefault values :
+'''
+joystick = JoyStick2D() #default 
+
+stop_flag = False
+while not stop_flag :
+	direction = joystick.get_direction()
+	left,right,up,down,stop = direction
+	print left,right,up,down,stop
