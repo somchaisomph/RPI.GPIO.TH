ab4a82b71769bff755b72c7153de266e483f67e6
Somchai Somphadung <somchaisomph@gmail.com>
2016-07-06 12:16:05 +0700

    Update joystics.py

--- gadgets/joystics.py
+++ gadgets/joystics.py
@@ -2,11 +2,11 @@ from spi.rpi_spi import rpi_spi_dev
 from spi.adc.MCP3208 import MCP3208
 
 class JoyStick2D():
-	def __init__(self,devic=0,swt_channel=0,x_channel=1,y_channel=2):
+	def __init__(self,device=0,swt_channel=0,x_channel=1,y_channel=2):
 		self.spi = rpi_spi_dev(device)
 		self.mcp = None
 		if self.spi is not None:
-			self.mcp = MCP3208(spi)	
+			self.mcp = MCP3208(self.spi)	
 		self.swt_channel = swt_channel
 		self.vrx_channel = x_channel
 		self.vry_channel = y_channel	
