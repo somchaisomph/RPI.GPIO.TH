301bd4929ba967fc0835bafd167c7e781ae9e93d
Somchai Somphadung <somchaisomph@gmail.com>
2016-07-07 11:48:19 +0700

    Update th_gpio.py

--- gadgets/th_gpio.py
+++ gadgets/th_gpio.py
@@ -15,7 +15,7 @@ class TH_GPIO():
     self._gpios = []
     atexit.register(self.all_done)
   
-  def init_gpio(self,pin_number,mode='in'):
+  def enable_pin(self,pin_number,mode='in'):
     if pin_number not in self._gpios :
       #should check valid pin number
       mode = lower(mode)
@@ -25,7 +25,7 @@ class TH_GPIO():
         GPIO.setup(self.gpio_number, GPIO.IN) 
       self._gpios.append(pin_number)
 
-  def init_gpios(self,pin_list=[],mode='in'):
+  def enable_pins(self,pin_list=[],mode='in'):
     mode = lower(mode)
     for p in pin_list:
       if p not in self._gpios :
@@ -36,16 +36,27 @@ class TH_GPIO():
       self._gpios.append(p)
 
   def send(self,pin_number,state=True):
-    if pin_number in self.gpio_number :
+    if pin_number in self._gpios :
       GPIO.output(pin_number,state)
   
   def read(self,pin_number):
     res = None
-    if pin_number in self.gpio_number :
+    if pin_number in self._gpios :
       res = GPIO.input(pin_number)
     return res
-      
-      
+
+  def disable_pin(self,pin_number):
+      if pin_number in self._gpios :
+          GPIO.cleanup(pin_number)
+          self._gpios.remove(pin_number)
+
+  def disable_pins(self,pin_list):
+      for p in pin_list:
+        if pin_number in self._gpios :
+          GPIO.cleanup(pin_number)
+          self._gpios.remove(p)
+          
   def all_done(self):
     GPIO.cleanup()
   
+TH_GPIO = singleton(TH_GPIO)  
