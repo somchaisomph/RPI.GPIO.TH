df2efe7cb4fc6bf7173b7d551e6bd6d6de0a7ce7
Somchai Somphadung <somchaisomph@gmail.com>
2016-07-08 09:17:59 +0700

    Update led.py

--- gadgets/led.py
+++ gadgets/led.py
@@ -7,24 +7,29 @@ class LED():
 		self.gpio_number = gpio_number
 		#A singleton class is treated like a function
 		TH_GPIO().enable_pin(gpio_number,mode='out')
+		self.status = 0 #0:off,1:on
     
 	def on(self):
-		TH_GPIO().send(self.gpio_number,1)
+		self.status = 1
+		TH_GPIO().send(self.gpio_number,self.status)
+		
     
 	def off(self):
-		TH_GPIO().send(self.gpio_number,0)
+		self.status = 0
+		TH_GPIO().send(self.gpio_number,self.status)
     
 	def blink(self,numTimes=3,speed=1):
+		self.status =  1
 		for i in range(0,numTimes):## Run loop numTimes
 			TH_GPIO().send(self.gpio_number,1)
 			time.sleep(speed)## Wait
 			TH_GPIO().send(self.gpio_number,0)
 			time.sleep(speed)## Wait
-
-				
+		self.status = 0
 
 	def cleanup(self):
 		TH_GPIO().disable_pin(self.gpio_number)
+		self.status = 0
 		
 class PWM_LED():
 	def __init__(self,gpio_number=7,freq=100):
