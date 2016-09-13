bd3f436a03480a476931e65d11758d859398843b
Somchai Somphadung <somchaisomph@gmail.com>
2016-09-13 12:56:40 +0700

    Update test_pir.py

--- test/test_pir.py
+++ test/test_pir.py
@@ -1,11 +1,11 @@
-from ...
+from gadgets.environment.motion import PIR
 
 if __name__ == "__main__":
 	try :
 		pir = PIR()
 		pir.start()	
 		while 1 :
-			print(pir.result())
+			print(pir.result()) # 1 : motion detected, 0: no motion detected
 	except KeyboardInterrupt :	
 		pir.stop()
 		pir.cleanup()
